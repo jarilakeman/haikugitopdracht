9f2d8b542eac530d039a864e6ff81758b4eafca6
Jari <35338@ma-web.nl>
2022-10-13 14:44:35 +0200

    dingen gedaan

--- haiku.py
+++ haiku.py
@@ -1,9 +1,24 @@
 
 
 #plak hier de haiku functions onder elkaar
+def Haiku1():
+    print("By: Jari")
+    print("The wren Earns his living Noiselessly.")
+    print("From time to time The clouds give rest To the moon-beholders.")
+    print("Over-ripe sushi, The Master Is full of regret.")
 
+def HaikuX():
+    print("By: ")
+    print("Line 1")
+    print("Line 2")
+    print("Line 3")
 
-
+def HaikuX():
+    print("By: ")
+    print("Line 1")
+    print("Line 2")
+    print("Line 3")
+    
 
 #laat de code hieronder met rust
 Haiku1()
